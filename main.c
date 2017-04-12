486f3d2ad641ba9f58a0263fa20c49f55ef4cead
UCyborg <nejc.staric.23@gmail.com>
2017-04-12 23:18:06 +0200

    Added dedicated server launcher

---
+++ main.c
@@ -0,0 +1,37 @@
+#define WIN32_LEAN_AND_MEAN
+#include <stdio.h>
+#include <Windows.h>
+
+int CALLBACK WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
+{
+	char szCmdLine[MAX_PATH];
+	STARTUPINFO sinfo;
+	PROCESS_INFORMATION pinfo;
+	DWORD exitcode;
+
+	if (*lpCmdLine)
+		_snprintf(szCmdLine, sizeof(szCmdLine), " -dedicated %s", lpCmdLine);
+	else
+		strcpy(szCmdLine, " -dedicated");
+
+	SetErrorMode(SEM_NOGPFAULTERRORBOX);
+
+	ZeroMemory(&sinfo, sizeof(sinfo));
+	sinfo.cb = sizeof(sinfo);
+	sinfo.dwFlags = STARTF_USESHOWWINDOW;
+	sinfo.wShowWindow = nCmdShow;
+
+	do
+	{
+		if (!CreateProcess("Drakan.exe", szCmdLine, NULL, NULL, FALSE, 0, NULL, NULL, &sinfo, &pinfo))
+			return 0;
+
+		WaitForSingleObject(pinfo.hProcess, INFINITE);
+		GetExitCodeProcess(pinfo.hProcess, &exitcode);
+
+		CloseHandle(pinfo.hProcess);
+		CloseHandle(pinfo.hThread);
+	} while (exitcode);
+
+	return 0;
+}
